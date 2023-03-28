b96251cd51b5b7ff531a52722e543a498a98b81c
Thaddeus Thomas <thomasthaddeus@cityuniversity.edu>
2023-03-28 17:11:47 -0600

    Add package dependencies

---
+++ Logging/Logger.cs
@@ -0,0 +1,52 @@
+﻿using NLog;
+using Nlog.Config;
+using NLog.Schema;
+using System;
+
+namespace Logging
+{
+    public static class Logger
+    {
+        private static readonly ILogger _logger = LogManager.GetCurrentClassLogger();
+
+        public static void Trace(string message)
+        {
+            _logger.Trace(message);
+        }
+
+        public static void Debug(string message)
+        {
+            _logger.Debug(message);
+        }
+
+        public static void Info(string message)
+        {
+            _logger.Info(message);
+        }
+
+        public static void Warn(string message)
+        {
+            _logger.Warn(message);
+        }
+
+        public static void Error(string message)
+        {
+            _logger.Error(message);
+        }
+
+        public static void Error(Exception ex, string message)
+        {
+            _logger.Error(ex, message);
+        }
+
+        public static void Fatal(string message)
+        {
+            _logger.Fatal(message);
+        }
+
+        public static void Fatal(Exception ex, string message)
+        {
+            _logger.Fatal(ex, message);
+        }
+    }
+}
--- TextToSpeech/TextToSpeech.cs
+++ TextToSpeech/TextToSpeech.cs
@@ -27,7 +27,7 @@ namespace TextToSpeech
         public async Task SynthesizeTextToSpeechAsync(string text, string outputFilePath)
         {
             var config = SpeechConfig.FromSubscription(SP_KEY, SP_REG);
-            using AudioConfig audioConfig = AudioConfig.FromWavFileOutput(outputFilePath);
+            using var audioConfig = AudioConfig.FromWavFileOutput(outputFilePath);
             using var synthesizer = new SpeechSynthesizer(config, audioConfig);
 
             var result = await synthesizer.SpeakTextAsync(text);
