48154337272f73dad905dad923d9d062d04c9111
Thaddeus Thomas <thomasthaddeus@cityuniversity.edu>
2023-03-28 17:37:28 -0600

    Rename Class

--- CommonUtilities/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace CommonUtilities
-{
-    public class Class1
-    {
-    }
-}
---
+++ CommonUtilities/FileUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace CommonUtilities
+{
+    public static class FileUtils
+    {
+        public static string GetTempFilePath(string extension)
+        {
+            string tempFileName = Path.GetRandomFileName();
+            string tempFilePath = Path.Combine(Path.GetTempPath(), tempFileName);
+
+            return Path.ChangeExtension(tempFilePath, extension);
+        }
+
+        public static void DeleteFileIfExists(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+        }
+
+        // Add more utility methods here as needed
+    }
+}
