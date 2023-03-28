2f2590a82c165ded183a84cd833da97b43beecfe
Thaddeus Thomas <thomasthaddeus@cityuniversity.edu>
2023-03-28 08:31:14 -0600

    Configuration and settings project added to solution

---
+++ Configuration/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace Configuration
+{
+    public class AppSettings
+    {
+        private readonly IConfigurationRoot _configuration;
+
+        public AppSettings(string configurationFilePath)
+        {
+            _configuration = new ConfigurationBuilder()
+                .SetBasePath(Path.GetDirectoryName(configurationFilePath))
+                .AddJsonFile(Path.GetFileName(configurationFilePath))
+                .Build();
+        }
+
+        public string GetCognitiveServicesSubscriptionKey()
+        {
+            return _configuration["CognitiveServices:SubscriptionKey"];
+        }
+
+        public string GetCognitiveServicesRegion()
+        {
+            return _configuration["CognitiveServices:Region"];
+        }
+        // Add more methods here to get other configuration settings as needed
+    }
+}
