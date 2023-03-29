ad8bbae5b3f0df6cfbe9ca2227fd6a991d0bbbca
Thaddeus Thomas <thomasthaddeus@cityuniversity.edu>
2023-03-28 22:10:17 -0600

    Logger update

--- Logging/Logger.cs
+++ Logging/Logger.cs
@@ -15,44 +15,46 @@ namespace Logging
 {
     public static class Logger
     {
+        // Create an instance of NLog logger
         private static readonly ILogger _logger = LogManager.GetCurrentClassLogger();
 
-        public static void Debug(string message)
-        {
+        // Log a Debug level message
+        public static void Debug(string message){
             _logger.Debug(message);
         }
 
-        public static void Error(string message)
-        {
+        // Log an Error level message
+        public static void Error(string message){
             _logger.Error(message);
         }
 
-        public static void Error(Exception ex, string message)
-        {
+        // Log an Error level message with an associated exception
+        public static void Error(Exception ex, string message){
             _logger.Error(ex, message);
         }
 
-        public static void Fatal(string message)
-        {
+        // Log a Fatal level message
+        public static void Fatal(string message){
             _logger.Fatal(message);
         }
 
-        public static void Fatal(Exception ex, string message)
-        {
+        // Log a Fatal level message with an associated exception
+        public static void Fatal(Exception ex, string message){
             _logger.Fatal(ex, message);
         }
 
-        public static void Info(string message)
-        {
+        // Log an Info level message
+        public static void Info(string message){
             _logger.Info(message);
         }
 
-        public static void Trace(string message)
-        {
+        // Log a Trace level message
+        public static void Trace(string message){
             _logger.Trace(message);
         }
-        public static void Warn(string message)
-        {
+
+        // Log a Warn level message
+        public static void Warn(string message){
             _logger.Warn(message);
         }
     }
